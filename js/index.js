aa7570a702447023945b8d9ab71495e0521a0248
KarlNersisyan <karlennersisyan814@gmail.com>
2021-06-20 02:36:47 +0400

    Added pictures and Changes in index.html files

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const burgerMenu = document.querySelector(".menu-mobile");
-const iconItem = document.querySelector("#burgerMenu");
+let burgerMenu = document.querySelector(".menu-mobile");
+let iconItem = document.querySelector("#burgerMenu");
 
 iconItem.addEventListener("click", () => {
   burgerMenu.classList.toggle("show");
@@ -7,10 +7,10 @@ iconItem.addEventListener("click", () => {
 
 // search
 function doSearch(id_form, id_camp) {
-  const search_string = $(id_camp).value;
+  let search_string = $(id_camp).value;
   search_string = trim(search_string);
 
-  const search_form = $(id_form);
+  let search_form = $(id_form);
   if (search_string.length < 3) {
     alert("Minimum 3 characters!");
     return false;
@@ -36,4 +36,39 @@ function myTrim(str, chars) {
 function myTrimFunc(str, chars) {
   chars = chars || "\\s";
   return str.replace(new RegExp("[" + chars + "]+$", "g"), "");
+}
+
+//-----------Slider-----
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
