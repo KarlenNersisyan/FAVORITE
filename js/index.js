612584e66050ea080fb78de6a9fbf156d38e1cfa
KarlNersisyan <karlennersisyan814@gmail.com>
2021-06-20 19:06:05 +0400

    Added new changes

--- js/index.js
+++ js/index.js
@@ -53,6 +53,30 @@ setInterval(() => {
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
+let img1 = document.querySelector("#img1");
+let img2 = document.querySelector("#img2");
+let img3 = document.querySelector("#img3");
+let img4 = document.querySelector("#img4");
+let img5 = document.querySelector("#img5");
+let img6 = document.querySelector("#img6");
+img1.addEventListener("click", () => {
+  currentSlide(1);
+});
+img2.addEventListener("click", () => {
+  currentSlide(2);
+});
+img3.addEventListener("click", () => {
+  currentSlide(3);
+});
+img4.addEventListener("click", () => {
+  currentSlide(4);
+});
+img5.addEventListener("click", () => {
+  currentSlide(5);
+});
+img6.addEventListener("click", () => {
+  currentSlide(6);
+});
 
 function showSlides(n) {
   let i;
@@ -75,3 +99,13 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
   captionText.innerHTML = dots[slideIndex - 1].alt;
 }
+
+let minus = document.querySelector("#minus-Slides");
+let plus = document.querySelector("#plus-Slides");
+
+minus.addEventListener("click", () => {
+  plusSlides(-1);
+});
+plus.addEventListener("click", () => {
+  plusSlides(1);
+});
